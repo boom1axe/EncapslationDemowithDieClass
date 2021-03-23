91b3e9f8086cfaa382d7fcf30120d5b10775c8d7
Dennis Kafton <boom1axe@att.net>
2021-03-23 08:05:48 -0700

    Add project files.

---
+++ EncapslationDemowithDieClass/Die.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapslationDemowithDieClass
+{
+    /// <summary>
+    /// A six sided die made from random int.
+    /// </summary>
+    class Die
+    {
+        // static fields are shared across
+        // all instances of a class
+        static Random rand;
+        byte minValue;
+        byte maxValue;
+
+        //Static Constructers are called once for all instances
+        static Die()
+        {
+            rand = new Random();
+        }
+
+        /// <summary>
+        /// Default dice is standard six sided dice
+        /// with value range of 1-6
+        /// </summary>
+        public Die():this(1, 6)
+        {
+            //minValue 1;
+            //minValue 6;
+            //Roll(); // Roll Die on creation to avoid default
+        }
+
+        /// <summary>
+        /// Creates a die with the numbers between the minimum
+        /// and maxmum values
+        /// </summary>
+        /// <param name="minValue">The inclusive lower bound</param>
+        /// <param name="maxvalue">the inclusive max bound</param>
+        public Die(byte minValue, byte maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+
+        /// <summary>
+        /// side of die
+        /// </summary>
+        public byte Value { get; private set; }
+
+        public bool IsHeld { get; set; }
+
+        /// <summary>
+        /// Roll a new random byte value
+        /// for a standard six sided die.
+        /// If the die is held, the currentvalue will be returned
+        /// and no new value generated
+        /// </summary>
+        /// <returns> a byte value betwwen 1-6 </returns>
+        public byte Roll()
+        {
+            // random use exclusive max value
+            // meaning true max value is offset by +1.
+            const int offSetExclusiveMax = 1;
+            if (!IsHeld) // if die is not held
+            {
+                Value = (byte)rand.Next(minValue, maxValue + offSetExclusiveMax);
+            }
+            
+            return Value;
+        }
+    }
+}
---
+++ EncapslationDemowithDieClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapslationDemowithDieClass
+{
+    class Program
+    {
+        /// <summary>
+        /// Desplays value of five dice (Yahtzee)
+        /// using console.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            Die[] dice = new Die[5];
+            for(int i = 0; i < dice.Length; i++)
+            {
+                dice[i] = new Die();
+                Console.WriteLine(dice[i].Value);
+            }
+
+            // Value cant be change becuse
+            // set is private in die class
+            //Die.Value = 6;
+        }
+    }
+}
